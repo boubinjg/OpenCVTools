f1ffc1c1ef908332b5e62a2bb3b0a5b49ade9454
boubinjg <boubinjg@gmail.com>
2018-01-20 16:13:59 -0500

    added progress bars

--- bgMaker.py
+++ bgMaker.py
@@ -3,6 +3,17 @@ import argparse
 from PIL import Image
 import os
 from os.path import isfile, join
+import sys
+
+def progress(count, total, status=''):
+        bar_len = 60
+        filled_len = int(round(bar_len * count / float(total)))
+        percents = round(100.0 * count / float(total), 1)
+
+        bar = '=' * filled_len + '-' * (bar_len-filled_len)
+        sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+        sys.stdout.flush()
+
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', help='The directory of your negative samples')
 
@@ -10,5 +21,9 @@ args = parser.parse_args()
 
 files = [f for f in os.listdir(args.dir) if isfile(join(args.dir, f))]
 bg = open('bg.txt', 'w')
+counter = 0
 for f in sorted(files): 
 	bg.write("%s/%s\n" % (args.dir, f));
+	counter += 1
+	progress(counter, len(files), "Writing")
+print("\nComplete")
--- infoMaker.py
+++ infoMaker.py
@@ -3,6 +3,8 @@ import argparse
 from PIL import Image
 import os
 from os.path import isfile, join
+import sys 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', help='The directory of your negative samples')
 parser.add_argument('width', help='width of your new sample')
@@ -10,12 +12,26 @@ parser.add_argument('height', help='height of your new sample')
 
 args = parser.parse_args()
 
+def progress(count, total, status=''):
+        bar_len = 60
+        filled_len = int(round(bar_len * count / float(total)))
+        percents = round(100.0 * count / float(total), 1)
+
+        bar = '=' * filled_len + '-' * (bar_len-filled_len)
+        sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+        sys.stdout.flush()
+
 size = int(args.width), int(args.height)
 files = [f for f in os.listdir(args.dir) if isfile(join(args.dir, f))]
 bg = open('info.dat', 'w')
+counter = 0
 for f in sorted(files): 
 	im = Image.open(args.dir + '/' +f)
 	img = im.resize(size, Image.ANTIALIAS)
 	#im = im.resize(size, Image.ANTIALIAS)
 	img.save(args.dir + '/' + f, "JPEG")
 	bg.write("%s/%s 1 0 0 %s %s\n" % (args.dir, f, args.width, args.height));
+	counter += 1
+	progress(counter, len(files), "Writing and Resizing");
+		
+print("\nComplete")
